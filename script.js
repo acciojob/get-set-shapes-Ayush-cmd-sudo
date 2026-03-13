0fae777034ffacc7e9f2c2a751fe420198920200
Ayush-cmd-sudo <ayu021singh@gmail.com>
2026-03-13 17:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+constructor(width,height){
+	this.x=width;
+	this.y=height;
+}
+width(){
+	console.log("width",this.x);
+}
+	height(){
+		console.log("height",this.y);
+	}
 
-class Square extends Animal {}
+getarea(){
+	console.log("The area of the rectangle is :",this.x*this.y);
+}
+	
+}
+
+class Square extends Rectangle {
+constructor(side){
+	super(side,side);
+}	
+getPerimeter(){
+	console.log(4*side);
+}
+
+
+
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
